df18c4de303e71204a999e1b6b525150fc4c4dc6
dmnielsen <dmnielsen@gmail.com>
2022-12-02 23:56:18 -0600

    day three; look at me

---
+++ aoc2022/day_03.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+import string
+from typing import List, Iterable, Dict
+from itertools import islice
+
+from aoc2022 import AOC_DIR
+from aoc2022.util import print_solutions
+
+INPUT_FILENAME = AOC_DIR / 'inputs' / '202203_input.txt'
+
+
+def load(filename: Path = INPUT_FILENAME) -> str:
+    with open(filename, 'r') as f:
+        return f.read()
+
+
+def parse_input(text: str) -> List[str]:
+    return text.strip().split('\n')
+
+
+def generate_priority_dict() -> Dict[str, int]:
+    lower = {l: ind for ind, l in enumerate(string.ascii_lowercase, start=1)}
+    upper = {l: ind for ind, l in enumerate(string.ascii_uppercase, start=27)}
+    return lower | upper
+
+
+def find_common_item(text: str) -> str:
+
+    if len(text) // 2 == 1:
+        print('odd')
+    mid_idx = len(text) // 2
+
+    left = text[:mid_idx]
+    right = text[mid_idx:]
+
+    return (set(left) & set(right)).pop()
+
+
+def find_badge_item(elf1: str, elf2: str, elf3: str) -> str:
+
+    return (set(elf1) & set(elf2) & set(elf3)).pop()
+
+
+def batched(iterable: Iterable[str], n: int) -> Iterable:
+    """Batch data into lists of length n. The last batch may be shorter.
+
+    Nicked from itertools recipes
+
+    Example
+    -------
+    > batched('ABCDEFG', 3) --> ABC DEF G
+    """
+    #
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while batch := list(islice(it, n)):
+        yield batch
+
+
+def solve_part1(input_: str) -> int:
+    priority_val = generate_priority_dict()
+    rucksacks = parse_input(input_)
+
+    common_item_priorities = [priority_val[find_common_item(rucksack)] for rucksack in rucksacks]
+
+    return sum(common_item_priorities)
+
+
+def solve_part2(input_: str) -> int:
+    priority_val = generate_priority_dict()
+    rucksacks = parse_input(input_)
+
+    badges = [priority_val[find_badge_item(*items)] for items in batched(rucksacks, 3)]
+
+    return sum(badges)
+
+
+def main(show_solution: bool = True):
+    input_ = load(INPUT_FILENAME)
+
+    result1 = solve_part1(input_)
+    result2 = solve_part2(input_)
+
+    if show_solution:
+        print_solutions(result1, result2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_day_03.py
@@ -0,0 +1,34 @@
+import pytest
+
+from aoc2022 import day_03 as day
+
+
+@pytest.fixture
+def mock_input():
+    return """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+
+
+def test_find_badge_item():
+    elf1 = 'vJrwpWtwJgWrhcsFMMfFFhFp'
+    elf2 = 'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL'
+    elf3 = 'PmmdzqPrVvPwwTWBwg'
+
+    expected = 'r'
+    result = day.find_badge_item(elf1, elf2, elf3)
+    assert expected == result
+
+def test_solve_part1(mock_input):
+    expected = 157
+    result = day.solve_part1(mock_input)
+    assert expected == result
+
+
+def test_solve_part2(mock_input):
+    expected = 70
+    result = day.solve_part2(mock_input)
+    assert expected == result
